fd364062bf7b449723fbd7bd857fa63510aa8a28
Ron Brons <ron.brons@rabobank.com>
2022-03-21 17:22:14 +0100

    module 3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
-import { addnewProduct} from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
+      .get(getProducts)
+    
       // post endpoint
       .post(addnewProduct);
+    
+     app.route('/products/:ProductID')
+      .get(getProductWithID);
 }
 
 export default routes;
