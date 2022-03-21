c9f07bfb4bcda540fa88032df8749bacfcc9e25a
Ron Brons <ron.brons@rabobank.com>
2022-03-21 11:28:34 +0100

    module 3

--- src/models/models.js
+++ src/models/models.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-export const ProductSchema = new Schema ({
+
+export const ProductSchema = new Schema({
   name: { 
     type: String, 
     required: true
